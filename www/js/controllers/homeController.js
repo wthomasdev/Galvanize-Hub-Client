fe9257a8bb512437545a6ba40bcb1d790378926e
Will Thomas <wlthomas001@gmail.com>
2016-09-14 09:54:26 -0600

    code modularized and all next train data being received

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -1,66 +1,47 @@
 angular
 .module('starter.controllers', [])
 .controller('homeController', function($scope, $state, dataFactory) {
-  var dayOfWeek = moment().isoWeekday();
-  var lineDay;
-  var currentTime = new Date().toLocaleTimeString('en-US', { hour12: false,
-                                             hour: "numeric",
-                                             minute: "numeric"});;
+var trainData;
+var currentTime = dataFactory.getCurrentTime();
+var dayOfWeek = dataFactory.getDayOfWeek();
+dataFactory.getAllTrains().then(function(data) {
+  trainData = data.data;
+  $scope.nextATrain = NextALineTrain(currentTime,trainData);
+  $scope.nextBTrain = NextBLineTrain(currentTime,trainData);
+  $scope.nextCTrain = NextCLineTrain(currentTime,trainData);
+  $scope.nextETrain = NextELineTrain(currentTime,trainData);
+  $scope.nextWTrain = NextWLineTrain(currentTime,trainData);
+  console.log($scope.nextATrain);
+  console.log($scope.nextBTrain);
+  console.log($scope.nextCTrain);
+  console.log($scope.nextETrain);
+  console.log($scope.nextWTrain);
 
-  currentTime = currentTime.replace(/\D/g,'');
-  switch(dayOfWeek) {
-    case 1:
-      dayOfWeek = 'MonThurs';
-      break;
-    case 2:
-      dayOfWeek = 'MonThurs';
-      break;
-    case 3:
-      dayOfWeek = 'MonThurs';
-      break;
-    case 4:
-      dayOfWeek = 'MonThurs';
-      break;
-    case 5:
-      dayOfWeek = 'Friday';
-      break;
-    case 6:
-      dayOfWeek = 'Saturday';
-      break;
-    case 7:
-      dayOfWeek = 'SundayHol';
-      break;
-    default:
-      dayOfWeek = 'MonThurs';
-  }
+})
 
-  function lineAppender (line, day) {
-    lineDay = line+day;
-    return lineDay
-  }
+function NextALineTrain(currentTime, trainData) {
+  var ALineTrain = trainData.ALine[dayOfWeek];
+  return dataFactory.getNextTrain(currentTime, ALineTrain)
+};
+function NextBLineTrain(currentTime, trainData) {
+  var BLineTrain = trainData.BLine[dayOfWeek];
+  return dataFactory.getNextTrain(currentTime, BLineTrain)
+};
+function NextCLineTrain(currentTime, trainData) {
+  var CLineTrain = trainData.CLine[dayOfWeek];
+  return dataFactory.getNextTrain(currentTime,CLineTrain);
+}
+function NextELineTrain(currentTime, trainData) {
+  var ELineTrain = trainData.CLine[dayOfWeek];
+  return dataFactory.getNextTrain(currentTime,ELineTrain);
+}
+function NextWLineTrain(currentTime, trainData) {
+  var WLineTrain = trainData.WLine[dayOfWeek];
+  return dataFactory.getNextTrain(currentTime,WLineTrain);
+}
 
-  lineAppender("ALine", dayOfWeek);
-
-  dataFactory.getAllTrains().then(function(data) {
-    var ALineTrainData = data.data[0];
-    var BLineTrainData = data.data[1];
-    var CLineTrainData = data.data[2];
-    var ELineTrainData = data.data[3];
-    var WLineTrainData = data.data[4];
-    var ALineTrains = ALineTrainData[lineDay];
-    console.log($scope.ALineTrains);
-
-    for (var i = 0; i < ALineTrains.length; i++) {
-      if (Number(currentTime) < Number(ALineTrains[i])) {
-        console.log(ALineTrains[i]);
-        return ALineTrains[i];
-      }
-    }
-  })
-  dataFactory.getAllMeetups().then(function(data) {
-    $scope.meetupData = data;
-    console.log($scope.meetupData);
-  })
-
-
+dataFactory.getAllMeetups().then(function(data) {
+  $scope.meetupData = data;
+  console.log($scope.meetupData);
+})
 })
--- www/js/factories.js
+++ www/js/factories.js
@@ -18,5 +18,58 @@ angular
     					return $q.resolve(results);
     				})
     			},
-        }
-    }])
+					getCurrentTime:
+					function () {
+						var currentTime = new Date().toLocaleTimeString('en-US', {
+						hour12: false,
+						hour: "numeric",
+						minute: "numeric"
+						});
+					  currentTime = currentTime.replace(/\D/g,'');
+						return currentTime;
+					},
+					getDayOfWeek:
+					function () {
+						var dayOfWeek = moment().isoWeekday();
+						switch(dayOfWeek) {
+					    case 1:
+					      dayOfWeek = 'MonThurs';
+					      break;
+					    case 2:
+					      dayOfWeek = 'MonThurs';
+					      break;
+					    case 3:
+					      dayOfWeek = 'MonThurs';
+					      break;
+					    case 4:
+					      dayOfWeek = 'MonThurs';
+					      break;
+					    case 5:
+					      dayOfWeek = 'Friday';
+					      break;
+					    case 6:
+					      dayOfWeek = 'Saturday';
+					      break;
+					    case 7:
+					      dayOfWeek = 'SundayHol';
+					      break;
+					    default:
+					      dayOfWeek = 'MonThurs';
+					  }
+						return dayOfWeek;
+					},
+					appendLineAndDay:
+					function (line, day) {
+					  var lineDay = line+day;
+					    return lineDay
+					  },
+					getNextTrain:
+					function (currentTime, trainData) {
+						for (var i = 0; i < trainData.length; i++) {
+							if (currentTime < trainData[i]) {
+								return trainData[i];
+							}
+						}
+					}
+				}
+    	}]);
